27e70b6d95fd7d6144a0fc2ade0fca27bc8cd90d
mcheng676 <73177196+mcheng676@users.noreply.github.com>
2020-11-16 22:25:30 -0500

    Create Program5.py

---
+++ Program5.py
@@ -0,0 +1,37 @@
+'''
+***PROGRAM 5***
+Write a program that prompts the user to enter an integer. The program will then print an isosceles triangle made of asterisks (*) with that leg length. 
+
+(Hints- Leg length 4 requires a total of 4 lines of asterisks:
+The 1st line will require 3 spaces followed by 1 asterisk.
+The 2nd line will require 2 spaces followed by 3 asterisks.
+The 3rd line will require 1 space followed by 5 asterisks.
+The 4th line will require no spaces followed by 7 asterisks.
+
+Leg length 5 requires a total of 5 lines of asterisks:
+The 1st line will require 4 spaces followed by 1 asterisk.
+The 2nd line will require 3 spaces followed by 3 asterisks.
+The 3rd line will require 2 spaces followed by 5 asterisks.
+The 4th line will require 1 space followed by 7 asterisks.
+The 5th line will require no spaces followed by 9 asterisks.
+
+You may find string replication (' ' * 3 ,  '*' * 5) particularly helpful here.)
+
+Examples of what should appear on the console when the program runs:
+
+Enter the leg length: 4
+
+   *
+  ***
+ *****
+*******
+   
+Enter the leg length: 5
+
+    *
+   ***
+  *****
+ *******
+*********
+'''
+
