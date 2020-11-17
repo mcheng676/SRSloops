9f13c1ab7ae3841222290daa9e049de46f6fe5f4
mcheng676 <73177196+mcheng676@users.noreply.github.com>
2020-11-16 22:27:28 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+program = int(input("Which program (1, 2, 3, 4, or 5)?"))
+
+if program == 1:
+  import Program1
+if program == 2:
+  import Program2
+if program == 3:
+  import Program3
+if program == 4:
+  import Program4
+if program == 5:
+  import Program5
