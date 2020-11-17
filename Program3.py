2ef681ee47ff0cf06b0499824f824b6e32d4f0fc
mcheng676 <73177196+mcheng676@users.noreply.github.com>
2020-11-16 22:00:54 -0500

    Create Program3.py

---
+++ Program3.py
@@ -0,0 +1,23 @@
+'''
+***PROGRAM 3***
+
+Write a program that repeatedly prompts the user to enter numbers until the user enters 0. The program will then count how many of the nonzero inputs 
+are even, and print the number of even inputs in a sentence.
+
+(Hint: For this part, it's likely that you will have an if block inside a while loop. Pay careful attention to the levels of indentation when that happens.)
+
+Example of what should appear on the console when this part runs:
+
+Enter a number or enter 0 to stop: 1
+Enter a number or enter 0 to stop: 2
+Enter a number or enter 0 to stop: 5
+Enter a number or enter 0 to stop: 6
+Enter a number or enter 0 to stop: 9
+Enter a number or enter 0 to stop: 10
+Enter a number or enter 0 to stop: 7
+Enter a number or enter 0 to stop: 0
+Number of evens: 3
+
+'''
+
+#start writing code below
